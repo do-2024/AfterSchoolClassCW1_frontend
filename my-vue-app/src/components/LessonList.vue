75a747ae4bc1556880a3951f055f575396465483
Daniel Oghene <67068782+do-2024@users.noreply.github.com>
2025-10-29 10:35:11 +0000

    template for LessonList component

---
+++ my-vue-app/src/components/LessonList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="lesson-grid">
+    <div v-for="lesson in lessons" :key="lesson.id" class="lesson-card">
+      <i :class="lesson.icon" class="icon"></i>
+      <h3>{{ lesson.subject }}</h3>
+      <p>{{ lesson.location }}</p>
+      <p>Price: £{{ lesson.price }}</p>
+      <p>Spaces: {{ lesson.spaces }}</p>
+      <button :disabled="lesson.spaces===0" @click="$emit('add-to-cart', lesson.id)">
+        Add to Cart
+      </button>
+    </div>
+  </div>
+</template>
+
