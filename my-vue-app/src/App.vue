4d844c57b58b247167471204dfb45edbd3b23d3c
Daniel Oghene <67068782+do-2024@users.noreply.github.com>
2025-10-29 10:45:34 +0000

    template for app component

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-container">
+    <header>
+      <h1>After School Classes</h1>
+      <div class="buttons">
+        <button @click="toggleCart">{{ showCart ? 'Back to Lessons' : 'View Cart (' + cart.length + ')' }}</button>
+        <button @click="reset">Reset</button>
+      </div>
+    </header>
+
+    <div v-if="showCart">
+      <Cart :cart="cart" @remove="removeFromCart" @checkout-success="onCheckoutSuccess" />
+    </div>
+
+    <div v-else>
+      <SearchBar @search="searchQuery = $event" />
+      <SortControls :sort-by="sortBy" :order-asc="orderAsc" @updateSort="updateSort" />
+      <LessonList :lessons="filteredLessons" @add-to-cart="addToCart" />
+    </div>
+  </div>
+</template>
+
+
