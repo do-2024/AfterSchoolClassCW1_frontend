c3372855d50b57501cdb1c929f210d7d1db4d12b
Daniel Oghene <67068782+do-2024@users.noreply.github.com>
2025-10-29 10:41:34 +0000

    template for sortcontrols component

---
+++ my-vue-app/src/components/SortControls.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="sort-controls">
+    <label>Sort by:</label>
+    <select v-model="internalSortBy" @change="emitChange">
+      <option value="subject">Subject</option>
+      <option value="location">Location</option>
+      <option value="price">Price</option>
+      <option value="spaces">Spaces</option>
+    </select>
+    <button @click="toggleOrder">
+      {{ internalOrderAsc ? '↑' : '↓' }}
+    </button>
+  </div>
+</template>
+
+
