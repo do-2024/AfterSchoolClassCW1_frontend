63f88654fce073a27c8be38c09d16d0549dc2986
Daniel Oghene <67068782+do-2024@users.noreply.github.com>
2025-10-29 10:31:12 +0000

    script for checkout component

--- my-vue-app/src/components/Checkout.vue
+++ my-vue-app/src/components/Checkout.vue
@@ -17,3 +17,29 @@
     </form>
   </div>
 </template>
+
+
+<script>
+export default {
+  name: 'Checkout',
+  props: ['cart'],
+  data() {
+    return { name: '', phone: '' }
+  },
+  methods: {
+    validName() {
+      return /^[A-Za-z ]+$/.test(this.name)
+    },
+    validPhone() {
+      return /^[0-9]+$/.test(this.phone)
+    },
+    submit() {
+      if (!this.validName() || !this.validPhone()) {
+        alert('Please enter a valid name and phone number.')
+        return
+      }
+      this.$emit('success')
+    }
+  }
+}
+</script>
