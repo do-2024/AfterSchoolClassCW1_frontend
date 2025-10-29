39923ee48f8cade185b181edacc8e4f656188882
Daniel Oghene <67068782+do-2024@users.noreply.github.com>
2025-10-29 10:38:25 +0000

    template for searchbar component

---
+++ my-vue-app/src/components/SearchBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="search-bar">
+    <input
+      type="text"
+      placeholder="Search lessons..."
+      v-model="query"
+      @input="$emit('search', query)"
+    />
+  </div>
+</template>
+
+
