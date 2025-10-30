207332b65eebd5ee533460dc34560cd4ad05b333
Daniel Oghene <67068782+do-2024@users.noreply.github.com>
2025-10-30 15:35:27 +0000

    script for sortcontrols component

--- afterschool-app/src/components/SortControls.vue
+++ afterschool-app/src/components/SortControls.vue
@@ -13,3 +13,28 @@ SortControls:                                                                   
   </div>
 </template>
 
+
+<script>
+export default {
+  name: 'SortControls',
+  props: ['sortBy', 'orderAsc'],
+  data() {
+    return {
+      internalSortBy: this.sortBy,
+      internalOrderAsc: this.orderAsc
+    }
+  },
+  methods: {
+    toggleOrder() {
+      this.internalOrderAsc = !this.internalOrderAsc
+      this.emitChange()
+    },
+    emitChange() {
+      this.$emit('updateSort', {
+        sortBy: this.internalSortBy,
+        orderAsc: this.internalOrderAsc
+      })
+    }
+  }
+}
+</script>
